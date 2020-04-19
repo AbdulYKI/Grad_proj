7d1b5ae3bed546394cac81d604c79985ccf79ab5
omareyad <huckeroo07@gmail.com>
2020-04-19 07:10:55 +0300

    postList

--- gradProj-spa/src/app/post/post.component.ts
+++ gradProj-spa/src/app/post/post.component.ts
@@ -7,6 +7,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { LanguageEnum } from "../helper/language.enum";
 
+import { Component, OnInit } from '@angular/core';
+import {faEye,faCaretUp,faCaretDown } from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: "app-post",
   templateUrl: "./post.component.html",
@@ -14,6 +16,35 @@ import { LanguageEnum } from "../helper/language.enum";
 })
 export class PostComponent implements OnInit {
   showEditorFlag = false;
+  status: boolean = false;
+  //Test object
+post_list = [{id:1,
+              username:'Ali',
+              imgUrl:'https://avatars2.githubusercontent.com/u/11575183?s=460&v=4',
+              title:'Fix Connection Asp.Net',
+              view:9000,
+              rank:1000,
+              yyyy:'10-July , 2020',
+              hours:'10 Pm',
+              category1:".Net",
+              category2:"Microsoft",
+              status:'careup',
+            }
+              ,
+              {id:1,
+                username:'Sara alex',
+                imgUrl:'https://avatars1.githubusercontent.com/u/3844427?s=460&v=4',
+                title:'Bootstrap 4 Padding left',
+                view:2000,
+                rank:200,
+                yyyy:'09-Feb , 2018',
+                hours:'03 Am',
+                category1:"Angualr",
+                category2:"Bootstrap 4",
+                status:'caerdown',
+              }
+
+            ]
 
   constructor(private sharedService: SharedService) {}
   get FaPlus() {
@@ -31,6 +62,15 @@ export class PostComponent implements OnInit {
   }
   hideEditor() {
     this.showEditorFlag = false;
+  }
+  get faEye(){
+    return faEye;
+  }
+  get faCaretUp(){
+    return faCaretUp;
+  }
+  get faCaretDown(){
+    return faCaretDown;
   }
 
   get Lexicon() {
