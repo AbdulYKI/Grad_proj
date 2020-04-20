3e957a8005344fcef2deb6aedec265756a0e463b
AbdulYKI <Abdulrahmanyki.1998@gmail.com>
2020-04-20 07:21:12 +0300

    post test

--- gradProj-spa/src/app/post/post.component.ts
+++ gradProj-spa/src/app/post/post.component.ts
@@ -6,7 +6,11 @@ import {
   faTimes,
 } from "@fortawesome/free-solid-svg-icons";
 import { LanguageEnum } from "../helper/language.enum";
-import {faEye,faCaretUp,faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEye,
+  faCaretUp,
+  faCaretDown,
+} from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: "app-post",
   templateUrl: "./post.component.html",
@@ -16,33 +20,34 @@ export class PostComponent implements OnInit {
   showEditorFlag = false;
   status: boolean = false;
   //Test object
-post_list = [{id:1,
-              username:'Ali',
-              imgUrl:'https://avatars2.githubusercontent.com/u/11575183?s=460&v=4',
-              title:'Fix Connection Asp.Net',
-              view:9000,
-              rank:1000,
-              yyyy:'10-July , 2020',
-              hours:'10 Pm',
-              category1:".Net",
-              category2:"Microsoft",
-              status:'careup',
-            }
-              ,
-              {id:1,
-                username:'Sara alex',
-                imgUrl:'https://avatars1.githubusercontent.com/u/3844427?s=460&v=4',
-                title:'Bootstrap 4 Padding left',
-                view:2000,
-                rank:200,
-                yyyy:'09-Feb , 2018',
-                hours:'03 Am',
-                category1:"Angualr",
-                category2:"Bootstrap 4",
-                status:'caerdown',
-              }
-
-            ]
+  post_list = [
+    {
+      id: 1,
+      username: "Ali",
+      imgUrl: "https://avatars2.githubusercontent.com/u/11575183?s=460&v=4",
+      title: "Fix Connection Asp.Net",
+      view: 9000,
+      rank: 1000,
+      yyyy: "10-July , 2020",
+      hours: "10 Pm",
+      category1: ".Net",
+      category2: "Microsoft",
+      status: "careup",
+    },
+    {
+      id: 1,
+      username: "Sara alex",
+      imgUrl: "https://avatars1.githubusercontent.com/u/3844427?s=460&v=4",
+      title: "Bootstrap 4 Padding left",
+      view: 2000,
+      rank: 200,
+      yyyy: "09-Feb , 2018",
+      hours: "03 Am",
+      category1: "Angualr",
+      category2: "Bootstrap 4",
+      status: "caerdown",
+    },
+  ];
 
   constructor(private sharedService: SharedService) {}
   get FaPlus() {
@@ -61,13 +66,13 @@ post_list = [{id:1,
   hideEditor() {
     this.showEditorFlag = false;
   }
-  get faEye(){
+  get faEye() {
     return faEye;
   }
-  get faCaretUp(){
+  get faCaretUp() {
     return faCaretUp;
   }
-  get faCaretDown(){
+  get faCaretDown() {
     return faCaretDown;
   }
 
