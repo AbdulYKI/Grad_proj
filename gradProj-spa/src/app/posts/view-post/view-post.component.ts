f1ed256e573953325d2cf5678ab1f31e0e157996
AbdulYKI <Abdulrahmanyki.1998@gmail.com>
2020-04-24 17:39:42 +0300

    few edits

--- gradProj-spa/src/app/posts/view-post/view-post.component.ts
+++ gradProj-spa/src/app/posts/view-post/view-post.component.ts
@@ -11,6 +11,19 @@ import { Post } from "src/app/models/post";
 export class ViewPostComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
   post: Post;
+  config: any = {
+    width: "100%",
+    base_url: "/tinymce",
+    suffix: ".min",
+    plugins:
+      // tslint:disable-next-line: max-line-length
+      "print preview  autoresize searchreplace autolink directionality visualblocks visualchars  image imagetools link media template codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists  wordcount   textpattern preview",
+    content_css: [],
+    directionality: "ltr",
+    toolbar: false,
+    menubar: false,
+    statusbar: false,
+  };
   ngOnInit(): void {
     this.route.data.subscribe((data: { post }) => (this.post = data.post));
   }
