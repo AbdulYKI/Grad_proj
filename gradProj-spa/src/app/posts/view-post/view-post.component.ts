9fcdd686a31aaf3429b2897570df8563d9ca4dd9
omareyad <huckeroo07@gmail.com>
2020-04-25 13:52:27 +0300

    view-post

--- gradProj-spa/src/app/posts/view-post/view-post.component.ts
+++ gradProj-spa/src/app/posts/view-post/view-post.component.ts
@@ -1,7 +1,14 @@
 import { ActivatedRoute } from "@angular/router";
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { Post } from "src/app/models/post";
-
+import {
+  faSortUp,
+  faSortDown,
+  faStar,
+  faCalendarAlt,
+  faEye,
+  faChevronLeft
+} from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: "app-view-post",
   templateUrl: "./view-post.component.html",
@@ -13,5 +20,23 @@ export class ViewPostComponent implements OnInit {
   post: Post;
   ngOnInit(): void {
     this.route.data.subscribe((data: { post }) => (this.post = data.post));
+  }
+  get FaSortUp() {
+    return faSortUp;
+  }
+  get FaSortDown() {
+    return faSortDown;
+  }
+  get Fastar() {
+    return faStar;
+  }
+  get FaCalender() {
+    return faCalendarAlt;
+  }
+  get FaEye() {
+    return faEye;
+  }
+  get FaChevronLeft() {
+    return faChevronLeft;
   }
 }
