940231ae68acad8441092a11c1414146a236a563
AbdulYKI <Abdulrahmanyki.1998@gmail.com>
2020-04-26 11:15:12 +0300

    removed sanitizer

--- gradProj-spa/src/app/posts/view-post/view-post.component.ts
+++ gradProj-spa/src/app/posts/view-post/view-post.component.ts
@@ -7,7 +7,7 @@ import {
   faStar,
   faCalendarAlt,
   faEye,
-  faChevronLeft
+  faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
 @Component({
   selector: "app-view-post",
@@ -18,6 +18,20 @@ import {
 export class ViewPostComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
   post: Post;
+
+  config: any = {
+    width: "100%",
+    base_url: "/tinymce",
+    suffix: ".min",
+    plugins:
+      // tslint:disable-next-line: max-line-length
+      "print preview  autoresize searchreplace autolink directionality visualblocks visualchars  image imagetools link media template codesample table charmap hr pagebreak nonbreaking anchor insertdatetime advlist lists  wordcount   textpattern preview",
+    content_css: [],
+    directionality: "ltr",
+    toolbar: false,
+    menubar: false,
+    statusbar: false,
+  };
   ngOnInit(): void {
     this.route.data.subscribe((data: { post }) => (this.post = data.post));
   }
--- grad_proj_api/Program.cs
+++ grad_proj_api/Program.cs
@@ -11,31 +11,38 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace grad_proj_api {
-    public class Program {
-        public static async Task Main (string[] args) {
-            var host = CreateHostBuilder (args).Build ();
-            using (var servicesScope = host.Services.CreateScope ()) {
-                var dbContext = servicesScope.ServiceProvider.GetRequiredService<DataContext> ();
-                await dbContext.Database.MigrateAsync ();
-                if (await dbContext.Countries.CountAsync () == 0) {
-                    var seeder = servicesScope.ServiceProvider.GetRequiredService<Seed> ();
-                    seeder.SeedCountries ();
+namespace grad_proj_api
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using (var servicesScope = host.Services.CreateScope())
+            {
+                var dbContext = servicesScope.ServiceProvider.GetRequiredService<DataContext>();
+                await dbContext.Database.MigrateAsync();
+                if (await dbContext.Countries.CountAsync() == 0)
+                {
+                    var seeder = servicesScope.ServiceProvider.GetRequiredService<Seed>();
+                    seeder.SeedCountries();
                 }
 
-                if (await dbContext.ProgrammingLanguages.CountAsync () == 0) {
-                    var seeder = servicesScope.ServiceProvider.GetRequiredService<Seed> ();
-                    seeder.SeedProgrammingLanguages ();
+                if (await dbContext.ProgrammingLanguages.CountAsync() == 0)
+                {
+                    var seeder = servicesScope.ServiceProvider.GetRequiredService<Seed>();
+                    seeder.SeedProgrammingLanguages();
                 }
 
             }
-            await host.RunAsync ();
+            await host.RunAsync();
         }
 
-        public static IHostBuilder CreateHostBuilder (string[] args) =>
-            Host.CreateDefaultBuilder (args)
-            .ConfigureWebHostDefaults (webBuilder => {
-                webBuilder.UseStartup<Startup> ();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
             });
     }
 }
