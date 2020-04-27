32a387a13d3f48546e4315679304e3c2109457f9
AbdulYKI <Abdulrahmanyki.1998@gmail.com>
2020-04-27 11:33:57 +0300

    format changes

--- grad_proj_api/Controllers/PostController.cs
+++ grad_proj_api/Controllers/PostController.cs
@@ -10,98 +10,115 @@ using grad_proj_api.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
-namespace DatingApp.API.Controllers {
-    [Route ("api/[controller]")]
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
     [ApiController]
 
     //Inheriting from ControllerBase gives us the ability to use http responses in our return values
     //and it helps with validation where without it we would have needed to use [FromBody] before our parameters and ModelState
-    public class PostController : ControllerBase {
+    public class PostController : ControllerBase
+    {
         private readonly IMainRepository _repo;
 
         private readonly IMapper _mapper;
 
-        public PostController (IMainRepository repo, IConfiguration configuration, IMapper mapper) {
+        public PostController(IMainRepository repo, IConfiguration configuration, IMapper mapper)
+        {
             _repo = repo;
 
             _mapper = mapper;
         }
 
-        [HttpPost ("{userId}")]
-        public async Task<IActionResult> AddPost (int userId, PostForAddDto postForAddDto) {
+        [HttpPost("{userId}")]
+        public async Task<IActionResult> AddPost(int userId, PostForAddDto postForAddDto)
+        {
 
-            try {
-                if (userId != int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value))
-                    return Unauthorized ();
-                var post = _mapper.Map<Post> (postForAddDto);
+            try
+            {
+                if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                    return Unauthorized();
+                var post = _mapper.Map<Post>(postForAddDto);
                 post.UserId = userId;
-                await _repo.Add (post);
+                await _repo.Add(post);
 
-                if (await _repo.SaveAll ()) {
-                    var postFromRepo = await _repo.GetPost (post.Id);
-                    var postToReturnDto = _mapper.Map<postToReturnDto> (postFromRepo);
-                    return CreatedAtAction (nameof (GetPost), new { id = post.Id }, postToReturnDto);
+                if (await _repo.SaveAll())
+                {
+                    var postFromRepo = await _repo.GetPost(post.Id);
+                    var postToReturnDto = _mapper.Map<postToReturnDto>(postFromRepo);
+                    return CreatedAtAction(nameof(GetPost), new { id = post.Id }, postToReturnDto);
                 }
 
-                return BadRequest ("Uploading post failed");
-            } catch (Exception exception) {
-                return BadRequest (exception.Message);
-
+                return BadRequest("Uploading post failed");
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
             }
 
         }
 
-        [HttpGet ("{id}", Name = nameof (GetPost))]
-        public async Task<IActionResult> GetPost (int id) {
-            var post = await _repo.GetPost (id);
+        [HttpGet("{id}", Name = nameof(GetPost))]
+        public async Task<IActionResult> GetPost(int id)
+        {
+            var post = await _repo.GetPost(id);
             if (post == null)
-                return NotFound ();
-            var postToReturn = _mapper.Map<postToReturnDto> (post);
-            return Ok (postToReturn);
+                return NotFound();
+            var postToReturn = _mapper.Map<postToReturnDto>(post);
+            return Ok(postToReturn);
         }
 
-        [HttpPut ("{userId}/{Id}")]
-        public async Task<IActionResult> UpdatePost (int userId, int id, PostForEditDto postForEditDto) {
-            if (userId != int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value)) {
-                return Unauthorized ();
+        [HttpPut("{userId}/{Id}")]
+        public async Task<IActionResult> UpdatePost(int userId, int id, PostForEditDto postForEditDto)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
             }
-            var postFromRepo = await _repo.GetPost (id);
+            var postFromRepo = await _repo.GetPost(id);
 
-            if (postFromRepo == null || postFromRepo.UserId != userId) {
-                return Unauthorized ();
+            if (postFromRepo == null || postFromRepo.UserId != userId)
+            {
+                return Unauthorized();
             }
-            _mapper.Map (postForEditDto, postFromRepo);
-            if (await _repo.SaveAll ()) {
-                return NoContent ();
+            _mapper.Map(postForEditDto, postFromRepo);
+            if (await _repo.SaveAll())
+            {
+                return NoContent();
             }
 
-            return BadRequest ("Failed To Update Post");
+            return BadRequest("Failed To Update Post");
 
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetPosts () {
-            var posts = await _repo.GetPosts ();
-            var postsToReturn = _mapper.Map<List<postToReturnDto>> (posts);
-            return Ok (postsToReturn);
+        public async Task<IActionResult> GetPosts()
+        {
+            var posts = await _repo.GetPosts();
+            var postsToReturn = _mapper.Map<List<postToReturnDto>>(posts);
+            return Ok(postsToReturn);
         }
 
-        [HttpDelete ("{userId}/{id}")]
-        public async Task<IActionResult> DeletePost (int userId, int id) {
+        [HttpDelete("{userId}/{id}")]
+        public async Task<IActionResult> DeletePost(int userId, int id)
+        {
 
-            if (userId != int.Parse (User.FindFirst (ClaimTypes.NameIdentifier).Value)) {
-                return Unauthorized ();
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
             }
-            var postFromRepo = await _repo.GetPost (id);
+            var postFromRepo = await _repo.GetPost(id);
 
-            if (postFromRepo == null || postFromRepo.UserId != userId) {
-                return Unauthorized ();
+            if (postFromRepo == null || postFromRepo.UserId != userId)
+            {
+                return Unauthorized();
             }
-            _repo.Delete (postFromRepo);
-            if (await _repo.SaveAll ()) {
-                return NoContent ();
+            _repo.Delete(postFromRepo);
+            if (await _repo.SaveAll())
+            {
+                return NoContent();
             }
-            return BadRequest ("Failed To delete Post");
+            return BadRequest("Failed To delete Post");
 
         }
 
