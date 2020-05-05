842da274e3eb08a9b61056bc17c957dac4421292
omareyad <huckeroo07@gmail.com>
2020-05-05 16:50:53 +0300

    Home-Page-Design

--- gradProj-spa/src/app/home/home.component.ts
+++ gradProj-spa/src/app/home/home.component.ts
@@ -1,11 +1,36 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, AfterViewInit } from "@angular/core";
 import { NgbModal, NgbModalOptions } from "@ng-bootstrap/ng-bootstrap";
+import Swiper from 'swiper';
+import {
+  faChevronRight
+} from "@fortawesome/free-solid-svg-icons";
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit ,AfterViewInit  {
   ngOnInit(): void {}
-  constructor() {}
+  constructor() {
+
+   }
+  //Swiper config on documents is Ready
+ngAfterViewInit(){
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
+
+get faChevronRight(){
+  return faChevronRight;
+}
+
 }
